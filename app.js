4f25054a1d152e47f35e461a175d1e040efe60a4
harrison0502 <harrison0502@gmail.com>
2023-06-28 17:08:49 +0800

    feat:Add views/main.hbs & index.hbs

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+const User=require('./models/user')
 const port = 3000
 require('./config/mongoose')
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 // setting routes
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { User })
 })
 // starts the express server and listening for connections.
 app.listen(port, () => {
