7b810775fac7f2d1a3cb3e4390a83aa763d2b93e
harrison0502 <harrison0502@gmail.com>
2023-06-28 19:53:22 +0800

    feat:Add README.md

---
+++ README.md
@@ -0,0 +1,40 @@
+# 登入系統
+
+## 介紹
+這是一個簡易的登入系統，能用白名單的帳號密碼登入。
+
+### 功能
+- 輸入email及密碼登入系統。
+
+## 開始使用
+1. 請先確認已安裝 Node.js 與 npm。
+2. 將專案 clone 到本地。
+3. 在本地開啟專案，進入資料夾後，在終端機輸入以下指令：
+```bash
+   npm install
+   ```
+4. 啟動伺服器，執行 app.js 檔案
+```bash
+   npm run dev
+   ```
+5. 當終端出現以下字樣，表示伺服器與資料庫已啟動並成功連結[http://localhost:3000/](http://localhost:3000/)
+```bash
+   Express app listening on port 3000
+   ```
+6. 若欲暫停伺服器
+ ```bash
+   ctrl + c
+   ```
+
+## 開發工具
+- Node.js 18.16.0,
+- express: 4.18.2,
+- express-handlebars: 4.0.2
+- nodemon: 2.0.22
+### 資料庫
+- Mongodb,
+- mongoose: 5.9.7
+
+
+### 開發人員
+[Harrison](https://github.com/Harrison0502)
--- app.js
+++ app.js
@@ -2,16 +2,41 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const User=require('./models/user')
+const bodyParser = require('body-parser')
 const port = 3000
 require('./config/mongoose')
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // setting routes
 app.get('/', (req, res) => {
   res.render('index', { User })
 })
+
+app.post('/', (req, res) => {
+  const email=req.body.email
+  const password=req.body.password
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        if (user.password === password) {
+          const name = user.firstName
+          res.render('login', {name})
+        } else {
+          res.render('index', { passwordWrong : true , email , password})
+        }
+      } else {
+        res.render('index', { userWrong: true, email , password })
+      }
+    })
+    .catch(error => {
+      res.send('Error'); 
+    });
+     
+})
+
 // starts the express server and listening for connections.
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}.`)
