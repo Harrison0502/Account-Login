ba0960a6bef1a18838dc7a71a172265fadd91bd3
harrison0502 <harrison0502@gmail.com>
2023-07-04 03:18:05 +0800

    feat:Add login and logout function with session and cookies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session');
 const app = express()
 const User=require('./models/user')
 const bodyParser = require('body-parser')
@@ -9,10 +10,18 @@ require('./config/mongoose')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(session({
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: false,
+}));
 // setting routes
 app.get('/', (req, res) => {
-  res.render('index', { User })
+  if (req.session.userId) {
+    res.render('login', { login: true, name: req.session.firstName })
+  } else {
+    res.render('index')
+  }
 })
 
 app.post('/', (req, res) => {
@@ -22,6 +31,9 @@ app.post('/', (req, res) => {
     .then(user => {
       if (user) {
         if (user.password === password) {
+          //若登入成功，將使用者的ID(資料庫內建)和名字 存儲到 session
+          req.session.userId = user._id
+          req.session.firstName = user.firstName
           const name = user.firstName
           res.render('login', {name})
         } else {
@@ -36,6 +48,18 @@ app.post('/', (req, res) => {
     });
      
 })
+
+app.get('/logout', (req, res) => {
+  // 清除相關的 session 資料
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    // 清除相關的 cookie
+    res.clearCookie('username')
+    res.redirect('/')
+  });
+});
 
 // starts the express server and listening for connections.
 app.listen(port, () => {
